84ce0a588ff044e59743b253cff002b323aa7519
ivanmaag <ivan.magarino.aguilar@gmail.com>
2026-09-17 10:02:59 +0200

    Se añaden los hooks useReducer

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,14 +1,15 @@
-import { useMemo } from "react"
+import { useMemo, type Dispatch } from "react"
 import type { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
+import type { OrderActions } from "../reducers/order-reducer"
 
 type OrderTotalProps = {
     order: OrderItem[],
     tip: number,
-    placeOrder: () => void
+    dispatch: Dispatch<OrderActions>
 }
 
-export default function OrderTotals({order, tip, placeOrder} : OrderTotalProps) {
+export default function OrderTotals({order, tip, dispatch} : OrderTotalProps) {
 
     const subtotalAmount = useMemo(() => order.reduce( (total, item) => total + (item.quantity * item.price), 0 ), [order])
     const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order]) // este cálculo se produce cuando cambia la propina o el pedido
@@ -34,7 +35,7 @@ export default function OrderTotals({order, tip, placeOrder} : OrderTotalProps) 
             <button
                 className="w-full bg-yellow-800 p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
                 disabled={totalAmount === 0}
-                onClick={placeOrder}
+                onClick={() => dispatch({type: 'place-order'})}
                 >
                     Guardar pedido
             </button>
--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -1,4 +1,5 @@
-import type { Dispatch, SetStateAction } from "react"
+import { type Dispatch } from "react"
+import type { OrderActions } from "../reducers/order-reducer"
 
 const tipOptions = [
   {
@@ -19,14 +20,14 @@ const tipOptions = [
 ]
 
 type TipPercentageFormProps = {
-    setTip: Dispatch<SetStateAction<number>>,
+    dispatch: Dispatch<OrderActions>
     tip: number
 }
 
-export default function TipPercentageForm({setTip, tip} : TipPercentageFormProps) {
+export default function TipPercentageForm({dispatch, tip} : TipPercentageFormProps) {
   return (
-    <div>
-        <h3 className="font-black text-2xl"></h3>
+    <div className="space-y-3">
+        <h3 className="font-black text-2xl">Propina</h3>
 
         <form>
             {tipOptions.map(tipOption => (
@@ -37,7 +38,7 @@ export default function TipPercentageForm({setTip, tip} : TipPercentageFormProps
                         type="radio"
                         name="tip"
                         value={tipOption.value}
-                        onChange={ e => setTip(+e.target.value)}
+                        onChange={ e => dispatch({type: 'add-tip', payload: {value: +e.target.value}})}
                         checked={tipOption.value === tip}
                     />
                 </div>
